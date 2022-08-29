16445642d3f4f9befab4ce657c1ca21cc8733a17
satyamshete <shetesatyam99@gmail.com>
2022-08-29 18:08:04 +0530

    [Satyam] Search Node In Binary Search Tree

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Binary serch Tree Add node and search node Program");
             Console.WriteLine("Enter 1 to Create Binary Search Tree");
             Console.WriteLine("Enter 2 to Add elements in Binary Search Tree");
+            Console.WriteLine("Enter 3 to Search element in Binary Search Tree");
             int Value = int.Parse(Console.ReadLine());
             BSTOperations<int> bSTOperations = new BSTOperations<int>();
 
@@ -31,6 +32,24 @@
                     bSTOperations.AddNode(16);
                     bSTOperations.AddNode(63);
                     bSTOperations.AddNode(67);
+                    break;
+                case 3:
+                    bSTOperations.AddNode(56);
+                    bSTOperations.AddNode(30);
+                    bSTOperations.AddNode(70);
+                    bSTOperations.AddNode(22);
+                    bSTOperations.AddNode(40);
+                    bSTOperations.AddNode(60);
+                    bSTOperations.AddNode(95);
+                    bSTOperations.AddNode(11);
+                    bSTOperations.AddNode(65);
+                    bSTOperations.AddNode(3);
+                    bSTOperations.AddNode(16);
+                    bSTOperations.AddNode(63);
+                    bSTOperations.AddNode(67);
+                    Console.WriteLine("Searching for Node 63");
+                    bSTOperations.SearchNode(63);
+
                     break;
                 default:
                     Console.WriteLine("Make Proper Selection");
